71b568d0219040a3ed17ea45d39335d1533af746
KonJud <deveuudes@gmail.com>
2024-01-26 01:43:00 +0100

    begin Account profile

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,6 +1,19 @@
 import AccountProfile from "@/components/forms/AccountProfile"
+import { currentUser } from '@clerk/nextjs'
 
 async function Page () {
+    const user = await currentUser()
+
+    const userInfo = {}
+
+    const userData = {
+        id: user?.id,
+        objectId: userInfo?._id,
+        username: userInfo?.username || user?.username,
+        name: userInfo?.name || user?.firstName || '',
+        bio: userInfo?.bio || "",
+        image: userInfo?.image || user?.imageUrl
+    }
     return (
         
         <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
@@ -8,7 +21,10 @@ async function Page () {
             <p className="mt-3 text-base-regular text-light-2">Complete your profile now to use Threads</p>
 
             <section className="mt-9 bg-dark-2 p-10">
-                <AccountProfile />
+                <AccountProfile 
+                    user={userData}
+                    btnTitle='continue'
+                />
             </section>
         </main>
         
--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,10 +1,73 @@
 'use client'
 
-const AccountProfile = () => {
+import { useForm } from "react-hook-form"
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { UserValidation } from "@/lib/validations/user"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { 
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '../ui/form'
+
+interface Props {
+    user: {
+        id: string
+        objectId: string
+        username: string 
+        name: string
+        bio: string
+        image: string
+    }
+    btnTitle: string
+}
+const AccountProfile = ({
+    user,
+    btnTitle
+}: Props) => {
+    const form = useForm({
+        resolver: zodResolver(UserValidation),
+        defaultValues: {
+            profile_photo: '',
+            name: '',
+            username: '',
+            bio: ''
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof UserValidation>) {
+        console.log(values)
+    }
     return(
-        <div>
-            Account Profile
-        </div>
+        <Form {...form}>
+            <form 
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+            >
+                <FormField 
+                    control={form.control}
+                    name='username'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription >
+                                This is your public display name.
+                            </FormDescription>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
     )
 }
 
